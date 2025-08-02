c62970b24a5543fe97b671b36533e2b24963f21f
Datta Madasu <70431425+datta30@users.noreply.github.com>
2025-08-02 10:54:32 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
   
   return (
     <>
-    <Router basename="/cicd-ecom">
+    <Router basename="/cicd-ecom-fork">
       <Routes>
         <Route path="/" element={<HomePage />}>
           <Route index element={<AboutUs />} />
